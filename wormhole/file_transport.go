60b34c6c852c0fb49023fc34f8530b8d692ea682
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2021-02-22 09:28:50 +0530

    dial string with formatting functions instead of concat'ing

--- wormhole/file_transport.go
+++ wormhole/file_transport.go
@@ -524,7 +524,8 @@ func (t *fileTransport) listenRelay() error {
 		// TODO: The hardcoding of the URL should be removed once there is
 		// a way to represent it in the Hint messages. At the moment, there
 		// is no way to do that and hence this hardcoding.
-		c, _, err := websocket.Dial(ctx, "ws://" + t.relayAddr + "/v1", nil)
+		wsRelayURL := fmt.Sprintf("ws://%s/v1", t.relayAddr)
+		c, _, err := websocket.Dial(ctx, wsRelayURL, nil)
 		if err != nil {
 			fmt.Errorf("websocket.Dial failed")
 		}
